72c6332db4b94545b98221daa2ab13f1b0cf2c16
Sourav <sourav.tphy@gmail.com>
2022-03-22 22:53:19 +0100

    reorganized files

--- __init__.py
+++
@@ -1,4 +0,0 @@
-from .main import QRdecomposition
-
-# that's the only class in the main module, and the only thing we want from this package pretty much
-# we can now use it like QRdecomp.QRdecomposition() without having to refer to the module main
